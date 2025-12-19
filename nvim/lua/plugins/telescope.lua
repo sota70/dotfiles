6226b1828dada7f5fe87e1c85e9d189bb9991b16
sota70 <sota52350@gmail.com>
2025-12-19 14:45:01 +0900

    changed telescope.nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,11 @@
--- plugins/telescope.lua:
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-     dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+        defaults = {
+            wrap_results = true,
+            sorting_strategy = 'ascending',
+            layout_strategy = 'vertical',
+        },
+    },
 }
