4c28c0fec231212e6caaa92fa737f2e28ba56fda
sota70 <sota52350@gmail.com>
2026-03-15 14:14:42 +0900

    changed telescope.nvim settings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,36 @@
+local actions = require('telescope.actions')
+
+-- word wrap in preview
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'TelescopePreviewerLoaded',
+    callback = function(args)
+        vim.wo.wrap = true
+        vim.wo.linebreak = true
+    end,
+})
+
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
         defaults = {
-            wrap_results = true,
             sorting_strategy = 'ascending',
-            layout_strategy = 'vertical',
+            wrap_results = true,
+            layout_strategy = 'horizontal',
+            layout_config = {
+                vertical = { width = 0.8 },
+                horizontal = { width = 0.95 },
+            },
+            mappings = {
+                n = {
+                    ['<C-j>'] = actions.preview_scrolling_down,
+                    ['<C-k>'] = actions.preview_scrolling_up,
+                },
+                i = {
+                    ['<C-j>'] = actions.preview_scrolling_down,
+                    ['<C-k>'] = actions.preview_scrolling_up,
+                },
+            },
         },
     },
 }
